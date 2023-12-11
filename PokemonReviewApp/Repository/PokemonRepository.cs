bea564a1249e94f9ec0d635f7b777c2c2ae47bf2
epan <114670023+mamokEpan@users.noreply.github.com>
2023-12-11 10:51:15 +0700

    first commit pokemon API

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,40 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExsists(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
